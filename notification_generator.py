230c94cd84739ffd2dbe095e2abadf7b208797e4
Karthic Palaniappan <karthic2507@gmail.com>
2023-10-04 19:04:19 -0400

    Added helpful docstrings for module and functions

--- notification_generator.py
+++ notification_generator.py
@@ -1,4 +1,29 @@
 #!/usr/bin/env python
+"""
+spo_portal_notifier
+
+This module automates the notification process for the placement portal
+at IIT Kanpur. It logs into the portal, checks for new notifications, and
+sends email notifications to the user if new posts are detected.
+
+Usage:
+1. Configure environment variables for email and portal login details.
+2. Run this module to check for new notifications.
+
+Functions:
+- read_dotenv(): Read environment variables for email and portal login details.
+- setup_logging(log_path): Set up logging to a file with the specified log path.
+- get_previous_post_id(post_id_path): Get the ID of the previous post from a file.
+- update_post_id(post_id_path, new_id): Update the previous post ID in a file.
+- notify_user(email, password): Notify the user via email.
+- main(): Main function to automate the placement portal notification process.
+
+Note:
+Ensure that the required environment variables are properly configured
+before running this module.
+
+Author: Karthic Palaniappan
+"""
 import os
 import smtplib
 import time
@@ -10,10 +35,16 @@ from selenium import webdriver
 from selenium.webdriver.support.ui import Select
 from selenium.webdriver.chrome.options import Options
 
-POST_ID_FILENAME = 'recent_post.txt',
+POST_ID_FILENAME = 'latest_post.log',
 LOGGING_FILENAME = 'spo_portal_notification.log'
 
 def read_dotenv():
+    """
+    Read environment variables for email and portal login details.
+
+    Returns:
+        dict: A dictionary containing EMAIL_ID, EMAIL_PWD, PORTAL_USR, and PORTAL_PWD.
+    """
     return {
         'EMAIL_ID': os.getenv("EMAIL_ID"),
         'EMAIL_PWD': os.getenv("EMAIL_PWD"),
@@ -22,10 +53,25 @@ def read_dotenv():
     }
 
 def setup_logging(log_path):
+    """
+    Set up logging to a file with the specified log path.
+
+    Args:
+        log_path (str): The path to the log file.
+    """
     logging.basicConfig(filename=log_path, level=logging.INFO, format='%(asctime)s %(message)s')
     print('Logging initialized')
 
 def get_previous_post_id(post_id_path):
+    """
+    Get the ID of the previous post from a file.
+
+    Args:
+        post_id_path (str): The path to the file storing the previous post ID.
+
+    Returns:
+        int: The previous post ID.
+    """
     if os.path.exists(post_id_path):
         with open(post_id_path, 'r') as f:
             prev_id = int(f.read())
@@ -36,10 +82,24 @@ def get_previous_post_id(post_id_path):
         return 0
 
 def update_post_id(post_id_path, new_id):
+    """
+    Update the previous post ID in a file.
+
+    Args:
+        post_id_path (str): The path to the file storing the previous post ID.
+        new_id (int): The new post ID to be stored.
+    """
     with open(post_id_path, 'w') as f:
         f.write(str(new_id))
 
 def notify_user(email, password):
+    """
+    Notify the user via email.
+
+    Args:
+        email (str): The user's email address.
+        password (str): The email account password.
+    """
     s = smtplib.SMTP(host='smtp-mail.outlook.com', port=587)
     s.starttls()
     s.login(email, password)
@@ -56,6 +116,9 @@ def notify_user(email, password):
     s.quit()
 
 def main():
+    """
+    Main function to automate the placement portal notification process.
+    """
     # load SPO portal login and email login details from ENV variables
     load_dotenv()
     env = read_dotenv()
@@ -96,7 +159,7 @@ def main():
         curr_id = int(recent_post_id)
         logging.info('Current ID: %s', curr_id)
 
-        # send email if previous test/interview differs from most recent one
+        # send email if new test/interview detected
         if curr_id > prev_id:
             update_post_id(POST_ID_PATH, curr_id)
             notify_user(env['EMAIL_ID'], env['EMAIL_PWD'])
